ecb2722a99ed2245d2c9421c13d4b0e0ede04899
Vijay-vk12 <vijaydev9030@gmail.com>
2023-10-10 13:37:31 +0530

    Spring Boot Project

---
+++ Student-Management-Web-App/src/main/java/com/sms/demo/StudentManagementWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sms.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementWebAppApplication.class, args);
+	}
+
+}
